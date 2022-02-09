1c40e8b60f822a70dcbc851046a575719c43641b
adrianshatte <37203377+adrianshatte@users.noreply.github.com>
2022-02-09 22:02:15 +1100

    Part 2 Phonebook server functionality
    Added server tasks for part 2 phonebook activity

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import personsService from "./services/phone-numbers.js"
 
 const App = () => {
 	const [persons, setPersons] = useState([])
@@ -11,28 +12,44 @@ const App = () => {
 	const [searchTerm, setSearchTerm] = useState("")
 
 	useEffect(() => {
-		axios //
-			.get("http://localhost:3001/persons")
-			.then((response) => {
-				setPersons(response.data)
-			})
+		personsService.getAll().then((initialPersons) => {
+			setPersons(initialPersons)
+		})
+		//axios //
+		//	.get("http://localhost:3001/persons")
+		//	.then((response) => {
+		//		setPersons(response.data)
+		//	})
 	}, [])
 
 	const addNewRecord = (event) => {
 		event.preventDefault()
 
+		const newPersonObj = {
+			name: newName,
+			number: newNumber,
+		}
+
 		if (persons.map((person) => person.name).includes(newName)) {
-			alert(`${newName} is already added to phonebook`)
-		} else {
-			const newPersonObj = {
-				name: newName,
-				number: newNumber,
+			const confirm = window.confirm(
+				`${newName} is already added to phonebook, replace the old number with a new one?`
+			)
+			if (confirm) {
+				const id = persons.find((person) => person.name === newName).id
+				personsService.update(id, newPersonObj).then((returnedPerson) => {
+					setPersons(
+						persons.map((person) =>
+							person.id !== id ? person : returnedPerson
+						)
+					)
+				})
 			}
-
-			setPersons(persons.concat(newPersonObj))
-
-			setNewName("")
-			setNewNumber("")
+		} else {
+			personsService.create(newPersonObj).then((returnedPerson) => {
+				setPersons(persons.concat(returnedPerson))
+				setNewName("")
+				setNewNumber("")
+			})
 		}
 	}
 
@@ -46,6 +63,18 @@ const App = () => {
 
 	const handleSearch = (event) => {
 		setSearchTerm(event.target.value)
+	}
+
+	const deletePerson = (event) => {
+		const name = event.target.getAttribute("data-name")
+
+		const confirm = window.confirm(`Delete ${name}?`)
+		if (confirm) {
+			const id = persons.find((person) => person.name === name).id
+			personsService.deleteRecord(id).then((deletedPerson) => {
+				setPersons(persons.filter((person) => person.id !== id))
+			})
+		}
 	}
 
 	const peopleToShow =
@@ -68,7 +97,7 @@ const App = () => {
 				numberHandler={handleNumberInput}
 			/>
 			<h2>Numbers</h2>
-			<Persons people={peopleToShow} />
+			<Persons people={peopleToShow} deleteHandler={deletePerson} />
 		</div>
 	)
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,18 +1,26 @@
 import React from "react"
 
-const Person = ({ name, number }) => {
+const Person = ({ name, number, deleteHandler }) => {
 	return (
 		<li>
-			{name} {number}
+			{name} {number}{" "}
+			<button type="button" data-name={name} onClick={deleteHandler}>
+				delete
+			</button>
 		</li>
 	)
 }
 
-const Persons = ({ people }) => {
+const Persons = ({ people, deleteHandler }) => {
 	return (
 		<ul>
 			{people.map((person) => (
-				<Person key={person.name} name={person.name} number={person.number} />
+				<Person
+					key={person.name}
+					name={person.name}
+					number={person.number}
+					deleteHandler={deleteHandler}
+				/>
 			))}
 		</ul>
 	)
